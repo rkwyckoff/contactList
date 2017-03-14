f164163b4aa83ec2be54df2f8a724a545f310cc6
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-14 15:05:35 -0400

    js index file changed

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,13 @@
 const ContactController = require("../controllers/contact");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 module.exports = (app) => {
+
+  app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+
   app.post('/contacts', ContactController.create);
   app.get('/contacts', ContactController.listContacts);
   app.get('/contacts/:id', ContactController.findContact);
